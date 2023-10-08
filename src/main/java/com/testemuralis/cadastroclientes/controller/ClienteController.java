4b289cc6c3c9c98dfd8bb1736d4388643a1391cc
Brian <brian.schneider.epica@gmail.com>
2023-10-08 03:35:17 -0300

    Correções em Atualizar Clientes

--- src/main/java/com/testemuralis/cadastroclientes/controller/ClienteController.java
+++ src/main/java/com/testemuralis/cadastroclientes/controller/ClienteController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,6 +56,14 @@ public class ClienteController {
 		Cliente clienteSalvo = clienteService.cadastrarCliente(cliente);
 		ClienteDTO clienteResponse = mapper.conversorClienteDTO(clienteSalvo);
 		return ResponseEntity.status(HttpStatus.CREATED).body(clienteResponse);
+	}
+	
+	@PutMapping
+	public ResponseEntity<ClienteDTO> put(@Valid @RequestBody ClienteDTO clienteRequest) {
+		Cliente cliente = mapper.conversorCliente(clienteRequest);
+		Cliente clienteSalvo = clienteService.atualizarCliente(cliente);
+		ClienteDTO clienteResponse = mapper.conversorClienteDTO(clienteSalvo);
+		return ResponseEntity.status(HttpStatus.OK).body(clienteResponse);
 	}
 	
 	@DeleteMapping("/{id}")
--- src/main/java/com/testemuralis/cadastroclientes/service/ClienteService.java
+++ src/main/java/com/testemuralis/cadastroclientes/service/ClienteService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.testemuralis.cadastroclientes.domain.model.Cliente;
 import com.testemuralis.cadastroclientes.domain.model.Contato;
@@ -28,11 +30,14 @@ public class ClienteService {
 		return clienteRepository.findById(id);
 	}
 	
-	/*public Cliente atualizarCliente (Cliente cliente) {
+	public Cliente atualizarCliente (Cliente cliente) {
 		
-		if(clienteRepository.existsById(cliente))
+		Optional<Cliente> clienteDadosAntigos = clienteRepository.findById(cliente.getId());
+		if (clienteDadosAntigos.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		return clienteRepository.save(cliente);
 		
-	} */
+	} 
 	
 	public void deletarCliente (Long id) {
 		clienteRepository.deleteById(id);
